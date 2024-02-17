31d0fd7e622ec937f8f6c258f30dde20e77bae20
cyberbeat03 <37319858+cyberbeat03@users.noreply.github.com>
2024-02-17 17:13:30 -0500

    Modified layout of MainWindow and added a context menu

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,72 +1,69 @@
 ﻿namespace MediaPlayerDemo.ViewModels;
 
 public partial class MainViewModel : MainViewModelBase
-{    
+{
     private DispatcherTimer _timer = new();
 
     public MainViewModel()
-    {
-        InitializePlayer();
-    }
-
-    private void InitializePlayer()
     {        
-                MPlayer.Volume = 0.5;
-        MPlayer.Balance = 0;
-        MPlayer.SpeedRatio = 1;
-        MPlayer.LoadedBehavior = MediaState.Manual;
-        MPlayer.MediaOpened += Media_Opened;
-        MPlayer.MediaEnded += Media_Ended;
-
         _timer.Interval = TimeSpan.FromSeconds(1);
         _timer.Tick += Timer_Tick;
-        GetMediaDetails();
+        MPlayer.MediaOpened += Media_Opened;
+        MPlayer.MediaEnded += Media_Ended;                
     }
 
     private void Timer_Tick(object? sender, EventArgs e)
     {
-        if (MPlayer.NaturalDuration.HasTimeSpan)        
-            ElapsedTime = MPlayer.Position.ToString(@"mm\:ss");        
+        if (MPlayer.NaturalDuration.HasTimeSpan)
+            ElapsedTime = MPlayer.Position.ToString(@"mm\:ss");
     }
 
-    private void GetMediaDetails()
+    private void PlayItem(MediaItem? currentItem)
     {
-        if (MPlayer.Source is null)
-            DisplayStatus = "There is currently no media loaded";
-        else
-            DisplayStatus = MPlayer.Source.OriginalString;
+        if (currentItem is not null && MPlayer.Source != currentItem.MediaUri)
+        {
+            MPlayer.Source = currentItem.MediaUri;
+            Play();
+        }
+        GetMediaDetails();
     }    
 
-[RelayCommand]
-    private void AddMediaFiles()
+    private void Media_Opened(object sender, RoutedEventArgs e)
     {
-       FileOpenService fileService = new();
-
-        IList<string> pickedFiles = fileService.PickMediaFiles();
-
-        if (pickedFiles.Count != 0)
-        {
-            CurrentMediaList.AddFiles(pickedFiles);
-            PlayItem(CurrentMediaList.CurrentItem);
-        }
+        TotalDuration = MPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
+        _timer.Start();
     }
-    
-[RelayCommand]
+
+    private void Media_Ended(object sender, RoutedEventArgs e)
+    {
+        _timer.Stop();
+        MPlayer.Stop();
+        ElapsedTime = "00:00";
+
+        if (CanRepeat)
+            Play();
+        else
+            Next();
+    }
+
+    [RelayCommand]
     private void Play()
     {
-        if (MPlayer.Source is null) return;
-
-        _timer.IsEnabled = true;
-        MPlayer.Play();
+        if (MPlayer.Source is not null)
+        {
+            _timer.IsEnabled = true;
+            MPlayer.Play();
+        }
     }
     
 [RelayCommand]
     private void Pause()
     {
-        if (MPlayer.Source is null) return;
-
-        _timer.IsEnabled = false;
-        MPlayer.Pause();
+        if (MPlayer.Source is not null)
+        {
+            _timer.IsEnabled = false;
+            MPlayer.Pause();
+        }
     }
 
 [RelayCommand]    
@@ -95,32 +92,18 @@ public partial class MainViewModel : MainViewModelBase
             PlayItem(CurrentMediaList.GetPreviousItem());        
     }
 
-    private void PlayItem(MediaItem? currentItem)
+    [RelayCommand]
+    private void AddMediaFiles()
     {
-        if (currentItem is not null && MPlayer.Source != currentItem.MediaUri)
+        FileOpenService fileService = new();
+
+        IList<string> pickedFiles = fileService.PickMediaFiles();
+
+        if (pickedFiles.Count != 0)
         {
-            MPlayer.Source = currentItem.MediaUri;
-            Play();
+            CurrentMediaList.AddFiles(pickedFiles);
+            PlayItem(CurrentMediaList.CurrentItem);
         }
-        GetMediaDetails();
-    }
-
-    private void Media_Opened(object sender, RoutedEventArgs e)
-    {
-        TotalDuration = MPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
-        _timer.Start();
-    }
-
-    private void Media_Ended(object sender, RoutedEventArgs e)
-    {
-        _timer.Stop();
-        MPlayer.Stop();
-        ElapsedTime = "00:00";
-
-        if (CanRepeat)        
-            Play();        
-        else        
-            Next();        
     }
 
 }
--- ViewModels/MainViewModelBase.cs
+++ ViewModels/MainViewModelBase.cs
@@ -8,4 +8,27 @@ public partial class MainViewModelBase : ObservableObject
     [ObservableProperty] string _totalDuration = "00:00";
     [ObservableProperty] string _elapsedTime = "00:00";
     [ObservableProperty] bool _canRepeat;
+
+    public MainViewModelBase()
+    {
+        InitializePlayer();    
+    }
+
+private     void InitializePlayer()
+    {
+        MPlayer.Volume = 0.5;
+        MPlayer.Balance = 0;
+        MPlayer.SpeedRatio = 1;
+        MPlayer.LoadedBehavior = MediaState.Manual;
+        GetMediaDetails();
+    }
+    
+  public void GetMediaDetails()
+    {
+        if (MPlayer.Source is null)
+            DisplayStatus = "There is currently no media loaded";
+        else
+            DisplayStatus = MPlayer.Source.OriginalString;
+    }    
+
 }
