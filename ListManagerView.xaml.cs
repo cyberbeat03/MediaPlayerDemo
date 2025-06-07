40394bf897e3114740c8f46aaaa57084b26cd5a3
cyberbeat03 <cyberbeat03@outlook.com>
2025-06-07 16:12:03 -0400

    Renamed files containing the view

---
+++ ListManagerView.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace WinMix;
+
+public partial class ListManagerView : Window
+{
+    public ListManagerView(ListManagerViewModel viewModel)
+    {
+        InitializeComponent();
+        DataContext = viewModel;
+        MediaListView.Focus();
+    }    
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+DialogResult = false;
+    }
+}
+
+
