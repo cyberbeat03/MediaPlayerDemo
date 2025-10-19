c07dbd01f1ef31c0bd2232f2f082c5668f6e61a0
cyberbeat03 <cyberbeat03@outlook.com>
2025-10-18 20:01:44 -0400

    Modified PlaybackService to ensure the exact index is removed when removing an item

--- Services/PlaybackService.cs
+++ Services/PlaybackService.cs
@@ -1,13 +1,13 @@
 ﻿namespace WinMix.Services;
 
 public class PlaybackService : IPlaybackService
-{    
+{
     public ObservableCollection<MediaItem> Items { get; } = new ObservableCollection<MediaItem>();
     public int CurrentIndex { get; set; } = -1;
     public string Name { get; set; } = "Untitled Playlist";
 
     private bool IsIndexValid(int index) =>
-        index >= 0 && index < Items.Count;    
+        index >= 0 && index < Items.Count;
 
     public MediaItem? GetCurrentItem() =>
         IsIndexValid(CurrentIndex) ? Items[CurrentIndex] : null;
@@ -35,37 +35,49 @@ public class PlaybackService : IPlaybackService
     }
 
     public IEnumerable<string> GetFilePaths()
-    {
-        var files = Items.Select(item => item.FullPath);
-        var validFiles = new List<string>();
-
-        foreach (var file in files)
+    {        
+        var paths = Items.Select(i => i.FullPath).ToList();
+        foreach (var path in paths)
         {
-            if (File.Exists(file))
-                validFiles.Add(file);
+            if (!string.IsNullOrWhiteSpace(path) && File.Exists(path))
+                yield return path;
         }
-
-        return validFiles;
-    }   
+    }
 
     public void AddItem(MediaItem item)
     {
-        if (item is null) return;
-        
-        Items.Add(item);
+        if (item is null)
+            return;
 
+        Items.Add(item);
         if (CurrentIndex <= -1 && Items.Count > 0)
             CurrentIndex = 0;
     }
 
     public void RemoveItem(MediaItem? itemToRemove)
     {
-        if (itemToRemove is null) return;
+        if (itemToRemove is null)
+            return;
 
-        Items.Remove(itemToRemove);
+        var removedIndex = Items.IndexOf(itemToRemove);
+        if (removedIndex < 0)
+            return;
+
+        Items.RemoveAt(removedIndex);
+
+        if (Items.Count == 0)
+        {
+            CurrentIndex = -1;
+            return;
+        }
+
+        if (removedIndex < CurrentIndex)
+        {
+            CurrentIndex--;
+            return;
+        }
 
         if (CurrentIndex >= Items.Count)
             CurrentIndex = Items.Count - 1;
     }
-
 }
